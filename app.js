b1d0f2a55befb13c0233982fbb3e26dd832bcb3e
Elvis <Elvis@Katherines-MacBook-Pro.local>
2020-02-07 13:44:46 -0700

    changes

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const request = require("request");
 const bodyParser = require("body-parser");
-const path = require("path");
+const now = new Date().toLocaleTimeString();
 
 const app = express();
 app.use(express.static("public"));
